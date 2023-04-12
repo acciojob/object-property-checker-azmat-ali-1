df61856c174f7d60147fea816cad5dc5c3be7204
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-12 16:53:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,16 +2,22 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-for(let key : sampleObject){
-    if (sampleObject[key]==key) {
+
+    if (sampleObject.hasOwnProperty(key)) {
     return true;
-		
 }
 else{
-	
-    return false;
+	    return false;
 }
 }
+
+
+
+
+
+
+
+
 
 
 // Do not change the code below
