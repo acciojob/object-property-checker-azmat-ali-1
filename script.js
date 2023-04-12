dd9af8680de43f0843e00fb453c53286964d0f09
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-04-12 16:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-if (key in window.sampleObject) {
+for(let key : sampleObject){
+    if (sampleObject[key]==key) {
     return true;
+		
 }
 else{
-    return false;
 	
+    return false;
 }
 }
+
 
 // Do not change the code below
 const key = prompt("Enter Key.");
